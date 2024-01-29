2454671168b2db654dd8e546dcbe94b78708ff5d
Chris Akiki <akikichriss@gmail.com>
2024-01-29 22:10:42 +0200

    ADD Clippath

---
+++ src/app/clippath/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import YellowVector from '@/SVG/YellowVector';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ShapeComponent = () => {
+  return (
+    <div className='relative w-64 h-64 rounded-full bg-yellow-400 m-8 clip-star'>
+      <div className='absolute inset-0  bg-yellow-400'></div>
+    </div>
+  );
+};
+
+export default function Home() {
+  const mainRef = useRef(null);
+  const svgRef = useRef<HTMLDivElement>(null);
+
+  const [windowWidth, setWindowWidth] = useState<number>();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.clippedImage',
+          start: 'top 90%',
+          end: 'bottom 0',
+          scrub: 1,
+          toggleActions: 'restart none none reset',
+          markers: true,
+        },
+      });
+      tl.to('.clippedImage', {
+        clipPath: 'circle(100% at 50% 50%)',
+        delay: 0.05,
+        ease: 'power2.out',
+      });
+      tl.to('.clippedImage', {
+        scale: 1.15,
+
+      }, 0.2)
+
+
+      return () => {};
+    });
+
+    return () => ctx.revert();
+  }, [windowWidth]);
+
+  return (
+    <div ref={mainRef}>
+      <div className='h-screen bg-red-500'></div>
+      <div
+        ref={svgRef}
+        className='h-[90vh] circle-animate flex items-center justify-center overflow-hidden relative bg-white'
+      >
+
+          <img
+            src='/crepaway1.jfif'
+            className='clippedImage w-full h-full object-cover'
+          />
+
+      </div>
+      <div className='h-screen'>
+
+      </div>
+    </div>
+  );
+}
